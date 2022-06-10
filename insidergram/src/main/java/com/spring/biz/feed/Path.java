245f996efadc2a16b0fa341ece4c44a50078b03e
bab <tjdghks1119@naver.com>
2022-06-10 15:32:08 +0900

    insertFeed

--- insidergram/src/main/java/com/spring/biz/feed/Path.java
+++ insidergram/src/main/java/com/spring/biz/feed/Path.java
@@ -1,7 +1,7 @@
 package com.spring.biz.feed;
 
 public class Path {
-	String path = "/Users/junhee/Programming/07_Spring/insidergram/insidergram/src/main/webapp/img_src/feed/";
+	String path = "/Users/babble/project/insidergram/insidergram/src/main/webapp/img_src/feed/";
 
 	public String getPath() {
 		return path;
